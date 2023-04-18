8f6528cc642e60113a28a167c91711ed05dc0d7a
manoti <manotimike@gmail.com>
2023-04-18 17:27:12 +0300

    completed successfully

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,16 +1,20 @@
-import React from "react";
-import BlogContent from "./BlogContent";
-import Comment from "./Comment";
+import React from 'react'
+import BlogContent from './BlogContent'
+import Comment from './Comment'
 
 function BlogPost() {
   return (
-    <div id="blog-post">
-      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
-      <Comment commentText="I agree with this statement. - Angela Merkel" />
-      <Comment commentText="A universal truth. - Noam Chomsky" />
-      <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+    <div id='blog-post'>
+      <BlogContent
+        articleText='Dear Reader: Bjarne Stroustrup has the perfect lecture oration.'
+        isPublished={true}
+        minutesToRead={1}
+      />
+      <Comment commentText='I agree with this statement. - Angela Merkel' />
+      <Comment commentText='A universal truth. - Noam Chomsky' />
+      <Comment commentText='Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451' />
     </div>
-  );
+  )
 }
 
-export default BlogPost;
+export default BlogPost
